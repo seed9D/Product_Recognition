2385d261150db8808628e36a0fff4757f719517a
CHIANGCHIUTI <sc510377@gmail.com>
2017-12-15 14:47:52 +0800

    add normalize wrod frequency

--- find_frequency_pattern.py
+++ find_frequency_pattern.py
@@ -5,6 +5,45 @@ import re
 import sys
 import operator
 from Tries import *
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+
+def normalize(_list, feature_range=(1, 255)):
+	key_list = []
+	value_list = []
+	for k, v in _list:
+		key_list.append(k)
+		value_list.append(v)
+
+	value_array = np.asarray(value_list, dtype=float).reshape(-1, 1)
+	scaler = MinMaxScaler(feature_range=feature_range)
+	value_array = scaler.fit_transform(value_array)
+
+	value_list = value_array.reshape(-1).tolist()
+	_list = []
+	for k, v in zip(key_list, value_list):
+		_list.append((k, v))
+	return scaler, _list
+
+
+def un_normalize(_list, scaler):
+	assert scaler
+	key_list = []
+	value_list = []
+	for k, v in _list:
+		key_list.append(k)
+		value_list.append(v)
+
+	value_array = np.asarray(value_list, dtype=float).reshape(-1, 1)
+	if scaler:
+		value_array = scaler.inverse_transform(value_array)
+		value_list = value_array.reshape(-1).tolist()
+		_list = []
+		for k, v in zip(key_list, value_list):
+			_list.append((k, int(v)))
+
+	return _list
 
 
 def read_data():
@@ -19,7 +58,7 @@ def read_data():
 			split_line = list(filter(None, split_line))
 			split_line = list(filter(lambda x: not x.isdigit(), split_line))
 			string_list.extend(split_line)
-		return set(string_list)
+		return string_list
 
 
 def write_data(_dict, file_name):
@@ -76,6 +115,29 @@ def get_longest_sentence(substring_list):
 	return longest_sentence
 
 
+def empty_node_by_suffix(tries, key_fre_pair):
+	'''
+	the length of key are ordered in DESC
+	'''
+	def set_node_count(tries, key):
+		match_word, node = tries.search(key)
+		assert(match_word == key)
+		if node:
+			node.count = 0
+	if not key_fre_pair:
+		return
+	_, key_fre_pair = normalize(key_fre_pair, feature_range=(1, 255))
+	for index, (current_key, current_fre) in enumerate(key_fre_pair):
+		if index + 1 == len(key_fre_pair):
+			break
+		next_key, next_fre = key_fre_pair[index + 1]
+		if abs(current_fre - next_fre) < current_fre * 0.2:
+			set_node_count(tries, next_key)
+
+		elif current_fre < next_fre and abs(current_fre - next_fre) > current_fre * 1.5:
+			set_node_count(tries, current_key)
+
+
 def empty_nodes_by_pefix(tries, key_fre_pair):
 	'''
 	the length of key are ordered in ASC
@@ -89,44 +151,25 @@ def empty_nodes_by_pefix(tries, key_fre_pair):
 
 	if not key_fre_pair:
 		return
-
+	_, key_fre_pair = normalize(key_fre_pair, feature_range=(1, 255))
 	for index, (current_key, current_fre) in enumerate(key_fre_pair):
+		if index + 1 == len(key_fre_pair):
+			break
 		next_key, next_fre = key_fre_pair[index + 1]
-		if abs(current_fre - next_fre) < current_fre * 0.1:  # similar
+		suffix_key_fre_pair = find_all_suffix_sentence(tries, current_key)
+		empty_node_by_suffix(tries, suffix_key_fre_pair)
+
+		if abs(current_fre - next_fre) < current_fre * 0.2:  # similar
 			set_node_count(tries, current_key)  # empty the key which is the shorter one
 
 		elif current_fre > next_fre and abs(current_fre - next_fre) > 0.4 * next_fre:
-			set_node_count(tries, next_key)
+			if index + 2 != len(key_fre_pair):
+				set_node_count(tries, next_key)
 
-		if index + 2 == len(key_fre_pair):
-			break
 	# print(key_fre_pair)
 	# print('')
 
 
-def empty_node_by_suffix(tries, key_fre_pair):
-	'''
-	the length of key are ordered in DESC
-	'''
-	def set_node_count(tries, key):
-		match_word, node = tries.search(key)
-		assert(match_word == key)
-		if node:
-			node.count = 0
-	if not key_fre_pair:
-		return
-	# print(key_fre_pair)
-	pre_fre = key_fre_pair[0][1]  # first key's frequency
-	for index, (key, fre) in enumerate(key_fre_pair):
-		if index == 0:
-			continue
-		if abs(pre_fre - fre) < fre * 0.1:
-			set_node_count(tries, key)
-
-		elif abs(pre_fre - fre) * 0.4 > pre_fre:
-			previous_key, previous_fre = key_fre_pair[index - 1]
-			set_node_count(tries, previous_key)
-		pre_fre = fre
 
 
 def filter_frequency_pattern():
@@ -138,7 +181,7 @@ def filter_frequency_pattern():
 			match = re.search('[a-zA-Z0-9]', k)  # key contain number or alphabet
 			if match:
 				continue
-			if v == 0:
+			if v <= 0:
 				continue
 			return_dict[k] = int(v)
 		return return_dict
@@ -146,20 +189,24 @@ def filter_frequency_pattern():
 	def print_prefix_sentence_fre(tries, sentence=None):
 		key_fre_list = find_all_prefix_sentence(tries, sentence)
 		print(key_fre_list)
-	
 
 	def print_suffix_sentence_fre(tries, sentence=None):
 		key_fre_list = find_all_suffix_sentence(tries, sentence)
 		print(key_fre_list)
 
 	pattern_dict = read_frequency_pattern()
-	# print(pattern_dict['胶套'])
+	# scaler, pattern_dict = normalize_dict(pattern_dict)
+
+	print('硅胶套:', pattern_dict['硅胶套'])
 	# print('pattern_dict length:', len(pattern_dict.keys()))
 	tries = build_tries_by_dict(pattern_dict)
 	key_list = tries.travel()
 	longest_sentence_list = get_longest_sentence(key_list)
 	test_sentence = '游乐设备充'
 	test_sentence = '厂家定做'
+	test_sentence = '自控飞机'
+	test_sentence = '大滑梯'
+	# test_sentence = '硅胶套'
 	print_prefix_sentence_fre(tries, test_sentence)
 	'''step 1'''
 	for longest_sentence in longest_sentence_list:
@@ -179,21 +226,22 @@ def filter_frequency_pattern():
 		empty_node_by_suffix(tries, suffix_key_fre_pair)
 
 	print_suffix_sentence_fre(tries, test_sentence)
-
+	print_prefix_sentence_fre(tries, test_sentence)
 
 	'''tries to dict'''
 	result_dict = {}
 	key_fre_pair = find_nodes_by_list(tries, key_list)
 	for key, fre in key_fre_pair:
 		result_dict[key] = int(fre)
+	# result_dict = un_normalize_dict(result_dict, scaler)
 	result_dict = filter_dict(result_dict)
-	print('胶套:', result_dict['胶套'])
+	print('硅胶套:', result_dict['硅胶套'])
 	write_data(result_dict, 'product_name.txt')
 
 
 def find_frequency_pattern():
 	batch_size = 40  # batch how many string in string list
-	threshold = 5  # the threshold for filtering the pattern frequency
+	threshold = 10  # the threshold for filtering the pattern frequency
 	clean_interval = 1000  # how often to clean patten frequency under threshold
 	# clean_size = 10000
 
@@ -230,11 +278,11 @@ def find_frequency_pattern():
 				pattern_dict = clean_under_threshold(pattern_dict, threshold)
 				# if index % clean_interval == 0 and index is not 0:
 
-		pattern_dict = clean_under_threshold(pattern_dict, 15)
+		pattern_dict = clean_under_threshold(pattern_dict, 20)
 		return pattern_dict
 
 	pattern_dict = {}
-	string_list = list(read_data())
+	string_list = read_data()
 	distict_substring_list_gen = get_all_distinct_substring(string_list)
 	pattern_dict = count_pattern_frequency(pattern_dict, distict_substring_list_gen)
 	write_data(pattern_dict, 'frequency_pattern.txt')
@@ -243,7 +291,6 @@ def find_frequency_pattern():
 if __name__ == '__main__':
 	# find_frequency_pattern()
 	filter_frequency_pattern()
-
 
 	# string_list = read_data()
 
