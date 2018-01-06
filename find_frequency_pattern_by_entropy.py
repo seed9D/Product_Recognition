5ebfb5a72592703248d5a4b0b33ce8c0d0a82d20
CHIANGCHIUTI <sc510377@gmail.com>
2018-01-06 23:26:19 +0800

    detail infromatoin for analysis

--- find_frequency_pattern_by_entropy.py
+++ find_frequency_pattern_by_entropy.py
@@ -10,10 +10,10 @@ import os
 import multiprocessing as mp
 
 split_word = r'[：；\-\+\(\)\\|，,《》（）【】/、&\s]'
-max_n_gram = 5
+max_n_gram = 8
 count_threshold = 10
-mutual_entropy_threshold =1e-4
-neighbor_entropy_threshold = 0.8
+mutual_entropy_threshold = 1e-6
+neighbor_entropy_threshold = 0.7
 cpu_num = mp.cpu_count()
 proccess_num = cpu_num - 1
 
@@ -38,6 +38,18 @@ def read_data():
         return string_list
 
 
+def write_detail(dict_):
+    sorted_key = sorted(dict_.keys())
+    with open(os.path.join('./usr', 'detail_information.txt'), 'w') as wf:
+        # for k, v in dict_.items():
+        for k in sorted_key:
+            v = dict_[k]
+            # key, joint_probability, mutual entropy, left_entropy, right_entropy
+            wf.write('{}\t{:.6f}\t{:.6f}\t{:6f}\t{:6f}\n'.format(k, v[0][0], v[0][1], v[0][2], v[0][3]))
+            for left, write, left_pro, right_pro, pro_multiply, MI in v[1:]:
+                wf.write('\t{}\t{}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\n'.format(
+                    left, write, left_pro, right_pro, pro_multiply, MI))
+        
 def approcimate_total_world_num(string_list):
     total_word_num = 0
     for string in string_list:
@@ -80,34 +92,48 @@ def count_pattern_frequency(n_gram_list):
     return pattern_dict
 
 
-def count_mutual_entropy(dict_, approcimate_total_word_num):
+def combine_detail_information(detail_dict, entropy_dict):
+    for key in entropy_dict.keys():
+        if key in detail_dict:
+            left_ent, right_ent = entropy_dict[key]
+            detail_dict[key][0].extend([left_ent, right_ent])
+
+def calculate_mutual_entropy(dict_, approcimate_total_word_num):
     def word_probability(word):
         # assert(word_len_statistic), 'word:{} frequency:{} len_statistic:{}'.format(word, dict_[word], word_len_statistic)
         return (dict_[word] + 1) / approcimate_total_word_num
-
     MI_dict = {}
+    detail_dict = defaultdict(list)
     for k, v in dict_.items():
-        if v > count_threshold and len(k) > 1:
+        if v > count_threshold and len(k) > 1:        
             marginal_probability = [word_probability(
                 k[:index]) * word_probability(k[index:]) for index in range(1, len(k))]
             marginal_probability = list(
-                filter(lambda x: x > 0, marginal_probability))
-            # print(marginal_probability)
-            # for index in range(1, len(k)):
-            #     mulply = word_probability(k[:index], each_gram_num) * word_probability(k[index:], each_gram_num)
-            #     print(k[:index], k[index:], mulply)
+                filter(lambda x: x > 0, marginal_probability))          
             if marginal_probability:
                 min_marginal_probability = min(marginal_probability)
                 join_probability = word_probability(k)
                 '''mutual entropy'''
                 mutual_entropy = sum(map(
                     lambda x: join_probability * math.log(join_probability / x), marginal_probability))
-
                 if mutual_entropy > mutual_entropy_threshold:
                     MI_dict[k] = mutual_entropy
-                # print('{} {:.4f}'.format(k, mutual_entropy))
+
+                    '''for detail'''
+                    # print('{} {:.4f}'.format(k, mutual_entropy))
+                    detail_dict[k].append([join_probability, mutual_entropy])
+                    for index in range(1, len(k)):
+                        left_pro = word_probability(k[:index])
+                        right_pro = word_probability(k[index:])
+                        pro_multpy = left_pro * right_pro
+                        MI = join_probability * math.log(join_probability / pro_multpy)
+                        detail_dict[k].append(
+                            [k[:index], k[index:],  left_pro, right_pro, pro_multpy, MI])
+                    #   print('\t {} {} {:.6f}'.format(k[:index], k[index:], MI))
+                    # print()
+
     print('len MI:{}'.format(len(MI_dict.keys())))
-    return MI_dict
+    return MI_dict, detail_dict
 
 
 def count_neighbor_entropy(key, mapping_list):
@@ -126,7 +152,7 @@ def count_neighbor_entropy(key, mapping_list):
     left_entropy = entropy([m[0] for m in match_list])
     right_entropy = entropy([m[1] for m in match_list])
     # print(key, min(left_entropy, right_entropy))
-    return (key, (min(left_entropy, right_entropy)))
+    return (key, left_entropy, right_entropy)
 
 
 def run_batch(*argc):
@@ -143,8 +169,9 @@ def run_batch(*argc):
     curerent_process = mp.current_process().name
     result_dict = {}
     for index, (key, mapping_list) in enumerate(key_mappping()):
-        k, free_degree = count_neighbor_entropy(key, mapping_list)
-        result_dict[k] = free_degree
+        k, left_entropy, right_entropy = count_neighbor_entropy(
+            key, mapping_list)
+        result_dict[k] = (left_entropy, right_entropy)
         if index % 250 == 0:
             print('process: {} {}/{}'.format(curerent_process, index, len(key_list)))
     return result_dict
@@ -198,6 +225,7 @@ def calculate_free_degree(string_list, dict_):
     print('len free degree', len(entropy_dict.keys()))
     return entropy_dict
 
+
 def find_frequency_pattern():
     string_list = read_data()
     # approcimate_total_word_num = approcimate_total_world_num(string_list)
@@ -205,17 +233,19 @@ def find_frequency_pattern():
     print('total world len', approcimate_total_word_num)
     distict_substring_list_gen = get_all_distinct_substring(
         string_list, max_n_gram)
-    pattern_dict = count_pattern_frequency(
-    distict_substring_list_gen)
-    MI_dict = count_mutual_entropy(pattern_dict, approcimate_total_word_num)
-    sort_entropy = sorted(MI_dict.items(),
-                          key=lambda x: x[1], reverse=True)
-    write_data(os.path.join('./usr', 'word_mutual_entropy.txt'),
-               ('\t'.join([key, str(ent)]) for key, ent in list(sort_entropy)))
+    pattern_dict = count_pattern_frequency(distict_substring_list_gen)
+    MI_dict, detail_dict = calculate_mutual_entropy(
+        pattern_dict, approcimate_total_word_num)
+    sort_entropy = sorted(MI_dict.items(), key=lambda x: x[1], reverse=True)
+    write_data(os.path.join('./usr', 'word_mutual_entropy.txt'), ('\t'.join([key, str(ent)]) for key, ent in list(sort_entropy)))
+
     entropy_dict = calculate_free_degree(string_list, MI_dict)
     sort_entropy = sorted(entropy_dict.items(), key=lambda x: x[1], reverse=True)
-    write_data(os.path.join('./usr', 'word_entropy.txt'),
-                ('\t'.join([key, str(ent)]) for key, ent in list(sort_entropy)))
+    write_data(os.path.join('./usr', 'word_entropy.txt'), ('\t'.join([key, str(left), str(right)]) for key, (left, right) in list(sort_entropy)))
+
+    del string_list, pattern_dict, sort_entropy
+    combine_detail_information(detail_dict, entropy_dict)
+    write_detail(detail_dict)
 
 if __name__ == '__main__':
     find_frequency_pattern()
